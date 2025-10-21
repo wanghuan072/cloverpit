3c408687343ed824a5c92c0d9183285ac30bbbc4
你的GitHub用户名 <GitHub>
2025-10-21 11:36:48 +0800

    优化性能-2

--- cloverpit/src/main.js
+++ cloverpit/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -7,7 +6,6 @@ import i18n from './i18n'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(i18n)
 
